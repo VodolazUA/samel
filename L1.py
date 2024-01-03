9f296f063bf1640bc61aba310f8bb2ddda928627
Oleksandr Yezhurov <sasha@bookmacPro.local>
2024-01-03 11:34:28 -0800

    task #2 has been added

--- L1.py
+++ L1.py
@@ -1,15 +1,21 @@
 
 class Vehicle:
     
+
     def __init__(self, name, age):
         self.name = name
+        self.__age = age
         print(f"New vehicle has been created: <{name}>")
 
     def __str__(self):
-        return f"{self.name}"
+        return f"{self.name} (age:{self.__age})"
 
     def drive(self):
         print(f"{self.name} is moving.")
+    
+    @property
+    def age(self):
+        return self.__age
     
     def stop(self):
         print(f"{self.name} has stopped.") 
@@ -20,6 +26,12 @@ class Vehicle:
 
 class Car(Vehicle):
 
+    def _is_engine_on(self):
+        return True
+    
+    def _are_wheels_rotating(self):
+        return True
+    
     def change_gear(self, new_gear):
         print(f"Current gear is {new_gear}.")
 
@@ -28,9 +40,19 @@ class Car(Vehicle):
       
 
 class ElectricCar(Car):
-        
+    
+    def _is_charged(self):
+        return True
+    
     def drive(self):
-        print(f"{self.name} is moving (eco).")
+        if (
+            self._is_charged() and 
+            self._is_engine_on() and 
+            self._are_wheels_rotating()
+        ):
+            print(f"{self.name} is moving (eco).")
+        else:
+            print('Oh nooooo')
 
 
 class GasCar(Car):
@@ -40,6 +62,8 @@ class GasCar(Car):
 
 
 if __name__ == "__main__":
+    
+    print("-----------")
 
     audi = ElectricCar("Audi e-tron", 3)
     audi.drive()
@@ -56,3 +80,10 @@ if __name__ == "__main__":
     tesla.change_gear("Reverse mode")
     tesla.stop()
     tesla.honk()
+
+    print(
+        f"{tesla} is older than {audi}: {tesla.is_older_than(audi)}"   
+    )
+    
+    print("Audi's age is:", audi.age)  # .age - это функция, которая задекорирована под проперти
+    # значение возраста хранится в car.__age
