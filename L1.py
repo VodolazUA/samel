a332f34a041cfacf756fa025350f76cb3a087727
Oleksandr Yezhurov <sasha@bookmacPro.local>
2024-01-04 10:38:31 -0800

    added a class atribute <operable>, filter method

--- L1.py
+++ L1.py
@@ -2,40 +2,57 @@
 
 class Vehicle:
     
-    def __init__(self, name, age):
+    def __init__(self, name: str, age: int, operable=True):
         self.name = name
+        self.move_statement = "is moving."
         self._age = age
-        print(f"New vehicle has been created: <{name}> (age: {self._age})")
+        self.__operable = operable
+        
+        print(f"New vehicle has been created: <{self.name}> (age: {self._age}), operable={self.__operable}")
 
     def __str__(self):
-        return f"{self.name} (age: {self._age})"
+        return f"{self.name}"
 
     def move(self):
-        print(f"{self.name} is moving.")
-    
+        
+        if self.__operable:
+            return self.move_statement
+        else:
+            return ""
+        
     def stop(self):
         print(f"{self.name} has stopped.") 
 
     def is_older_than(self, car):
         return self._age > car._age
+    
+    def get_unoperable(vehicles):
+        unoperable = []
+        for v in vehicles:
+            if v.move() == "":
+                unoperable.append(v)
+        return unoperable
 
 
 class Plane(Vehicle):
     
-    def move(self):
-        print(f"{self.name} is flying.")
+    def __init__(self, name: str, age: int, operable=True):
+        super().__init__(name, age, operable)
+        self.move_statement = "is flying."
     
 
 class Boat(Vehicle):
     
-    def move(self):
-        print(f"{self.name} is sailing.")
+    def __init__(self, name: str, age: int, operable=True):
+        super().__init__(name, age, operable)
+        self.move_statement = "is sailing."
 
 
 class Car(Vehicle):
 
-    def move(self):
-        print(f"{self.name} is driving.")
+    def __init__(self, name: str, age: int, operable=True):
+        super().__init__(name, age, operable)
+        self.move_statement = "is driving."
 
     def _is_engine_on(self):
         return True
@@ -52,6 +69,10 @@ class Car(Vehicle):
 
 class ElectricCar(Car):
     
+    def __init__(self, name: str, age: int, operable=True):
+        super().__init__(name, age, operable)
+        # self.move_statement = f"{self.name} is driving (eco)."
+    
     def _is_charged(self):
         return True
     
@@ -61,9 +82,9 @@ class ElectricCar(Car):
             self._is_engine_on() and 
             self._are_wheels_rotating()
         ):
-            print(f"{self.name} is moving (eco).")
+            return "is driving (super mega eco green)."
         else:
-            print('Oh nooooo')
+            return ""
 
 
 class GasCar(Car):
@@ -76,9 +97,17 @@ if __name__ == "__main__":
     
     vehicles = []
     vehicles.append(Boat("Pirogoff", 47))
-    vehicles.append(Plane("Boeing 777", 12))
+    vehicles.append(Plane("Boeing 777", 12, False))
     vehicles.append(ElectricCar("Tesla Model 3 Dual Motor", 2))
     vehicles.append(GasCar("Audi RS6", 1))
     
-    for v in vehicles:
-        v.move()
+    print("\n---------\n")
+
+    for vehicle in vehicles:
+        print(vehicle, vehicle.move())
+
+    # відбір ТЗ які НЕ на ходу
+    unoperable = Vehicle.get_unoperable(vehicles)
+    print("\nThere is a list of unoperable vehicles:")
+    for uv in unoperable:
+        print("-", uv)
